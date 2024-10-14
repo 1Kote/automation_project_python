6c7b1cd752aed58cea0b88f4a91cd316decb3e73
1Kote <thepvpgod1@gmail.com>
2024-10-14 00:02:45 -0300

    continuar dps

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Test')
+import pyautogui 
+import time
+#Remember: pyoutogui.write -> write a string text
+#Remember: pyoutogui.press -> press especific keyboard key
+#Remember: pyoutogui.click -> click mouse1
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press('win')
+pyautogui.write('opera')
+pyautogui.press('enter')
+pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/tabela')
+pyautogui.press('enter')
+time.sleep(2)
